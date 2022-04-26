87a015769cf6de1298f6d58295a5fe42678a99d2
Cadu Grillo <cadugrillo@gmail.com>
2022-04-26 16:32:42 -0400

    updated url endpoint

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gateway: 'http://ec2-54-147-217-157.compute-1.amazonaws.com',
+  gateway: 'http://50.16.98.124',
   cognito: {
     userPoolId: 'us-east-1_wXNyjVJP4',
     userPoolWebClientId: '3bjl2vgb74537vk0b6r33r7jp4',
