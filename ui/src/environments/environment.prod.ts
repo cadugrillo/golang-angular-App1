23c498c90265de0ff8653c98f5a5340024a7174a
Cadu Grillo <cadugrillo@gmail.com>
2022-08-18 16:55:13 -0400

    changed environment variable for production

--- ui/src/environments/environment.prod.ts
+++ ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  gateway: 'http://50.16.98.124',
+  gateway: 'http://' + window.location.hostname,
   cognito: {
     userPoolId: 'us-east-1_wXNyjVJP4',
     userPoolWebClientId: '3bjl2vgb74537vk0b6r33r7jp4',
