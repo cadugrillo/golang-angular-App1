935f289d5589a0fa744ec165ff11da8ca595580a
cadugrillo <cadugrillo@gmail.com>
2022-09-18 00:57:04 -0400

    improved mqtt client methods

--- ui/src/app/mqtt-client/mqtt-client.component.ts
+++ ui/src/app/mqtt-client/mqtt-client.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
 import { Subscription } from 'rxjs';
 import { IMqttMessage } from "ngx-mqtt";
 import { MqttClientService } from '../mqttClient.service';
 import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MessagePopupComponent } from '../message-popup/message-popup.component';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-mqtt-client',
@@ -19,35 +22,63 @@ import { MatTableDataSource } from '@angular/material/table';
 })
 export class MqttClientComponent implements OnInit, OnDestroy {
 
-  messages: IMqttMessage[] = []
+  messages: IMqttMessage[] = [];
+  recTS: string[] = [];
   subscription!: Subscription;
-  topic: string = "#"
+  topic: string = '';
+  running: boolean = false;
   dataSource!: MatTableDataSource<IMqttMessage>;
+
 
   columnsToDisplay = ['Topic', 'Timestamp'];
   columnsToDisplayWithExpand = [...this.columnsToDisplay, 'expand'];
   expandedElement!: IMqttMessage | null;
 
-  constructor(private mqttClientService: MqttClientService) {}
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private mqttClientService: MqttClientService, public dialog: MatDialog,) {}
 
   ngOnInit(): void {
     this.dataSource = new MatTableDataSource();
-    this.subscribeToTopic();
   }
 
   ngOnDestroy(): void {
-    this.subscription.unsubscribe();  
+    
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+    
+    this.running = false;
+    this.messages = [];
+    this.recTS = [];  
 }
 
-  subscribeToTopic() {
-    this.subscription = this.mqttClientService.topic(this.topic).subscribe((data: IMqttMessage) => {
-      data.payload = JSON.parse(data.payload.toString());
-      this.messages.push(data);
-      this.dataSource.data = this.messages;
-      console.log(this.messages);
-      
-    });
+  subscribeToTopic(topic: string) {
+    if (this.topic != '') {
+      this.running = true;
+      this.subscription = this.mqttClientService.topic(topic).subscribe((data: IMqttMessage) => {
+        console.log('Initial time:'+this.getTimestamp());
+        data.payload = JSON.parse(data.payload.toString());
+        this.messages.push(data);
+        this.recTS.push(this.getTimestamp());
+        this.dataSource = new MatTableDataSource(this.messages);
+        this.dataSource.paginator = this.paginator;
+        console.log('Final time:'+this.getTimestamp());
+      });
+    } else this.dialog.open(MessagePopupComponent, {data: {title: "Error", text: "Topic field cannot be empty!"}});
   }
+
+  unsubscribeTopic() {
+    this.subscription.unsubscribe();
+    this.running = false; 
+  }
+
+  clearData() {
+    this.dataSource = new MatTableDataSource();
+    this.messages = [];
+    this.recTS = [];
+  }
+
   toString(payload: Object): string {
     return JSON.stringify(payload, null, 4);
   }
@@ -55,7 +86,7 @@ export class MqttClientComponent implements OnInit, OnDestroy {
   getTimestamp(): string {
     var today = new Date();
     var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()+ ":" + today.getMilliseconds();
     var dateTime = date+' '+time;
     return dateTime;
   }
