c9e7e27689abd088c0c35b66ac8f2dc866716f71
Cadu Grillo <cadugrillo@gmail.com>
2022-09-19 13:54:33 -0400

    debugging

--- ui/src/app/mqtt-client/mqtt-client.component.ts
+++ ui/src/app/mqtt-client/mqtt-client.component.ts
@@ -7,6 +7,7 @@ import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MessagePopupComponent } from '../message-popup/message-popup.component';
 import { MatDialog } from '@angular/material/dialog';
+import { saveAs } from "file-saver";
 
 @Component({
   selector: 'app-mqtt-client',
@@ -28,6 +29,8 @@ export class MqttClientComponent implements OnInit, OnDestroy {
   topic: string = '';
   running: boolean = false;
   dataSource!: MatTableDataSource<IMqttMessage>;
+  exportedData: ExportedData[] = [];
+  exportedDataItem: ExportedData = {topic: '', payload: ''}
 
 
   columnsToDisplay = ['Topic', 'Timestamp'];
@@ -58,9 +61,8 @@ export class MqttClientComponent implements OnInit, OnDestroy {
       this.running = true;
       this.subscription = this.mqttClientService.topic(topic).subscribe((data: IMqttMessage) => {
         //console.log('Initial time:'+this.getTimestamp());
-        data.payload = JSON.parse(data.payload.toString());
         this.messages.push(data);
-        this.recTS.push(this.getTimestamp());
+        this.recTS.push(this.getTimestamp("display"));
         this.dataSource = new MatTableDataSource(this.messages);
         this.dataSource.paginator = this.paginator;
         //console.log('Final time:'+this.getTimestamp());
@@ -80,14 +82,53 @@ export class MqttClientComponent implements OnInit, OnDestroy {
   }
 
   toString(payload: Object): string {
-    return JSON.stringify(payload, null, 4);
+    return JSON.stringify(JSON.parse(payload.toString()), null, 2);
   }
 
-  getTimestamp(): string {
-    var today = new Date();
-    var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()+ ":" + today.getMilliseconds();
-    var dateTime = date+' '+time;
-    return dateTime;
+  getTimestamp(format: string): string {
+
+    switch(format) {
+      case "display":
+        var today = new Date();
+        var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+        var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds()+ ":" + today.getMilliseconds();
+        var dateTime = date+' '+time;
+        return dateTime;
+      case "file":
+        var today = new Date();
+        var date = today.getFullYear()+'_'+(today.getMonth()+1)+'_'+today.getDate();
+        var time = today.getHours() + "_" + today.getMinutes() + "_" + today.getSeconds()+ "_" + today.getMilliseconds();
+        var dateTime = date+'_'+time;
+        return dateTime;
+    }
+    return "err"
   }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  exportMessages() {
+    
+    this.exportedData = [];
+     
+    this.messages.forEach((message) => {
+      let exportedDataItem = new ExportedData
+      exportedDataItem.topic = message.topic;
+      exportedDataItem.payload = JSON.parse(message.payload.toString());
+      this.exportedData.push(exportedDataItem);
+    });
+    
+    return saveAs(new Blob([JSON.stringify(this.exportedData, null, 2)], { type: 'JSON' }), 'messages_'+this.getTimestamp("file")+'.json');
+  }
+}
+
+class ExportedData {
+  topic!: Object
+  payload!: Object
 }
